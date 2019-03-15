aeaa5494bf569fcb081ae53c35c3dea811b2c901
Igor Silva <psilva.igor@gmail.com>
2019-03-14 22:44:36 -0300

    Modificando o retorno do serviço

--- src/main/java/com/igorsilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/igorsilva/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,14 @@
 package com.igorsilva.cursomc.resources;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.igorsilva.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -16,7 +20,16 @@ public class CategoriaResource implements Serializable {
 	private static final long serialVersionUID = -3971382025645401918L;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+
+		List<Categoria> categorias = new ArrayList<Categoria>();
+
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+
+		return categorias;
 	}
 }
