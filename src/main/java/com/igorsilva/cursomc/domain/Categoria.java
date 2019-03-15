acca2cf32ef63167ad310010bbfb9a0a45a6148f
Igor Silva <psilva.igor@gmail.com>
2019-03-14 23:07:08 -0300

    Adicionando o banco de dados e criação automática das tabelas

--- src/main/java/com/igorsilva/cursomc/domain/Categoria.java
+++ src/main/java/com/igorsilva/cursomc/domain/Categoria.java
@@ -2,6 +2,12 @@ package com.igorsilva.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	/**
@@ -9,6 +15,8 @@ public class Categoria implements Serializable {
 	 */
 	private static final long serialVersionUID = 1554851770193458651L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
