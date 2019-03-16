a101576f8bcde38aaff0791e4490cd152950481a
Igor Silva <psilva.igor@gmail.com>
2019-03-15 22:55:59 -0300

    Modelando a parte de estado e cidade

--- src/main/java/com/igorsilva/cursomc/CursomcApplication.java
+++ src/main/java/com/igorsilva/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.igorsilva.cursomc.domain.Categoria;
+import com.igorsilva.cursomc.domain.Cidade;
+import com.igorsilva.cursomc.domain.Estado;
 import com.igorsilva.cursomc.domain.Produto;
 import com.igorsilva.cursomc.repositories.CategoriaRepository;
+import com.igorsilva.cursomc.repositories.CidadeRepository;
+import com.igorsilva.cursomc.repositories.EstadoRepository;
 import com.igorsilva.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +21,15 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -30,18 +40,31 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 
-		Produto produto1 = new Produto(null,"Computador", 2000.00);
-		Produto produto2 = new Produto(null,"Impressora", 800.00);
-		Produto produto3 = new Produto(null,"Mouse", 80.99);
-		
-		categoria1.getProdutos().addAll(Arrays.asList(produto1,produto2,produto3));
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.99);
+
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
 		categoria2.getProdutos().addAll(Arrays.asList(produto2));
-		
+
 		produto1.getCategorias().addAll(Arrays.asList(categoria1));
 		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		produto3.getCategorias().addAll(Arrays.asList(categoria1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 	}
 }
